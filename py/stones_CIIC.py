fe97061f8f56203490f92b44285f901ef61f950e
Florian Thiery <web@florian-thiery.de>
2021-01-01 18:59:14 +0100

    correct stone script

--- py/stones_CIIC.py
+++ py/stones_CIIC.py
@@ -22,9 +22,13 @@ import sys
 # set UTF8 as default
 importlib.reload(sys)
 
+# set starttime
+starttime = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+
 # set paths
 dir_path = os.path.dirname(os.path.realpath(__file__))
 file_in = dir_path.replace("\\py", "\\data_raw\\CIIC_Ireland\\csv") + "\\" + "OghamStonesIreland.csv"
+file_in2 = "https://github.com/FellowsFreiesWissen/Ogham/blob/main/data_raw/CIIC_Ireland/csv/" + "OghamStonesIreland.csv"
 file_out = dir_path.replace("\\py", "\\ttl") + "\\" + "OghamStonesIreland.ttl"
 
 # read csv file
@@ -44,7 +48,7 @@ lines = []
 for index, row in data.iterrows():
     # print(lineNo)
     tmpno = lineNo - 2
-    if tmpno % 10 == 0:
+    if tmpno % 100 == 0:
         print(tmpno)
     lineNo += 1
     lines.append("ogham:stone_ciic_" + str(row['P1545_ciic']) + " " + "rdf:type" + " oghamonto:Stone .")
@@ -52,32 +56,49 @@ for index, row in data.iterrows():
     lines.append("ogham:stone_ciic_" + str(row['P1545_ciic']) + " " + "dc:identifier" + " " + "'" + str(row['P1545_ciic']) + "'" + ".")
     # geom
     lines.append("ogham:stone_ciic_" + str(row['P1545_ciic']) + " " + "geosparql:hasGeometry" + " ogham:stone_ciic_" + str(row['P1545_ciic']) + "_geom .")
-    lines.append("ogham:stone_ciic_" + str(row['P1545_ciic']) + "_geom " + "rdf:type" + " sf:the_geom .")
+    lines.append("ogham:stone_ciic_" + str(row['P1545_ciic']) + "_geom " + "rdf:type" + " sf:Point .")
     split = str(row['P625_coord']).split(",")
     point = "POINT(" + str(split[1]) + " " + str(split[0]) + ")"
     point = "\"<http://www.opengis.net/def/crs/EPSG/0/4326> " + point + "\"^^geosparql:wktLiteral"
     lines.append("ogham:stone_ciic_" + str(row['P1545_ciic']) + "_geom " + "geosparql:asWKT " + point + ".")
+    # prov-o
+    lines.append("ogham:stone_ciic_" + str(row['P1545_ciic']) + " " + "prov:wasAttributedTo" + " ogham:PythonStonesCIIC .")
+    lines.append("ogham:stone_ciic_" + str(row['P1545_ciic']) + " " + "prov:wasDerivedFrom" + " <https://github.com/FellowsFreiesWissen/Ogham/blob/main/data_raw/CIIC_Ireland/csv/OghamStonesIreland.csv> .")
+    lines.append("ogham:stone_ciic_" + str(row['P1545_ciic']) + " " + "prov:wasGeneratedBy" + " ogham:activity_stone_ciic_" + str(row['P1545_ciic']) + " .")
+    lines.append("ogham:activity_stone_ciic_" + str(row['P1545_ciic']) + " " + "rdf:type" + " <http://www.w3.org/ns/prov#Activity> .")
+    lines.append("ogham:activity_stone_ciic_" + str(row['P1545_ciic']) + " " + "prov:startedAtTime '" + starttime + "'^^xsd:dateTime .")
+    lines.append("ogham:activity_stone_ciic_" + str(row['P1545_ciic']) + " " + "prov:endedAtTime '" + datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%fZ") + "'^^xsd:dateTime .")
+    lines.append("ogham:activity_stone_ciic_" + str(row['P1545_ciic']) + " " + "prov:wasAssociatedWith" + " ogham:PythonStonesCIIC .")
     lines.append("")
 
 files = (len(lines) / 100000) + 1
-print("lines", len(lines), "files", int(files))
+print("triples", len(lines), "files", int(files))
 
 # write output file
 print("start writing turtle file...")
 file = codecs.open(file_out, "w", "utf-8")
-file.write("# create triples from " + file_in + " \r\n")
+file.write("# create triples from " + file_in2 + " \r\n")
 file.write("# on " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M") + "\r\n\r\n")
-prefixes = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> \r\nPREFIX owl: <http://www.w3.org/2002/07/owl#> \r\nPREFIX xsd: <http://www.w3.org/2001/XMLSchema#> \r\nPREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> \r\nPREFIX geosparql: <http://www.opengis.net/ont/geosparql#> \r\nPREFIX dc: <http://purl.org/dc/elements/1.1/> \r\nPREFIX sf: <http://www.opengis.net/ont/sf#> \r\n"
-prefixes += "PREFIX oghamonto: <http://ontology.ogham.link/> \r\nPREFIX ogham: <http://lod.ogham.link/data/> \r\n"
+prefixes = ""
+prefixes += "@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .\r\n"
+prefixes += "@prefix owl: <http://www.w3.org/2002/07/owl#> .\r\n"
+prefixes += "@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .\r\n"
+prefixes += "@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .\r\n"
+prefixes += "@prefix geosparql: <http://www.opengis.net/ont/geosparql#> .\r\n"
+prefixes += "@prefix dc: <http://purl.org/dc/elements/1.1/> .\r\n"
+prefixes += "@prefix sf: <http://www.opengis.net/ont/sf#> .\r\n"
+prefixes += "@prefix prov: <http://www.w3.org/ns/prov#> .\r\n"
+prefixes += "@prefix oghamonto: <http://ontology.ogham.link/> .\r\n"
+prefixes += "@prefix ogham: <http://lod.ogham.link/data/> .\r\n"
 prefixes += "\r\n"
 file.write(prefixes)
 for line in lines:
     file.write(line)
     file.write("\r\n")
-
-print("Yuhu!")
 file.close()
+
 print("*****************************************")
 print("SUCCESS")
 print("closing script")
+print(len(lines), "Triples")
 print("*****************************************")
