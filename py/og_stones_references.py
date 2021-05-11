c8d1ff7a9330937d7ba5b2f85afc2889f3b244b8
Florian Thiery <web@florian-thiery.de>
2021-05-11 17:31:08 +0200

    stone references pt.1

--- py/og_stones_references.py
+++ py/og_stones_references.py
@@ -6,7 +6,7 @@ __version__ = "beta"
 __maintainer__ = "Florian Thiery"
 __email__ = "mail@fthiery.de"
 __status__ = "beta"
-__update__ = "2021-05-09"
+__update__ = "2021-05-11"
 
 # import dependencies
 import uuid
@@ -19,6 +19,7 @@ import datetime
 import importlib
 import sys
 import hashlib
+import _config
 
 # set UTF8 as default
 importlib.reload(sys)
@@ -37,7 +38,7 @@ data = pd.read_csv(
     file_in,
     encoding='utf-8',
     sep=',',
-    usecols=['id', 'original_id', 'stone_type', 'cisp_id', 'o3d', 'macalister_1945', 'macalister_1907', 'mcmanus_1991', 'macalister_1902', 'macalister_1897', 'cuppage_1986', 'macalister_1949', 'brash_1879', 'osullivan_1996', 'power_1992', 'macalister_1909', 'petrie_1872', 'brikil_1993', 'raftery_1960', 'okasha_forsyth_2001', 'ferguson_1887', 'power_1997', 'g_id', 'ciic_stone', 'cisp_stone', 'o3d_stone'],
+    usecols=['g_id', 'id', 'original_id', 'stone_type', 'cisp_id', 'o3d', 'macalister_1945', 'macalister_1907', 'mcmanus_1991', 'macalister_1902', 'macalister_1897', 'cuppage_1986', 'macalister_1949', 'brash_1879', 'osullivan_1996', 'power_1992', 'macalister_1909', 'petrie_1872', 'brikil_1993', 'raftery_1960', 'okasha_forsyth_2001', 'ferguson_1887', 'power_1997', 'ciic_stone', 'cisp_stone', 'o3d_stone'],
     na_values=['.', '??', 'NULL']  # take any '.' or '??' values as NA
 )
 print(data.info())
@@ -52,11 +53,30 @@ for index, row in data.iterrows():
         print(tmpno)
     lineNo += 1
     # info
-    lines.append("ogham:Y" + str(row['id']) + " " + "rdf:type" + " oghamonto:OghamStone .")
+    lines.append("_:" + str(row['g_id']) + " " + "rdf:type" + " oghamonto:ReferenceChain .")
+    if int(str(row['id'])) > 10000000 and int(str(row['id'])) < 20000000:
+        lines.append("ogham:ciic:" + str(row['id']) + " " + "rdf:type" + " oghamonto:" + "macalister_1945" + " .")
+        lines.append("ogham:ciic:" + str(row['id']) + " " + "rdfs:label" + " 'ciic:" + str(row['id']) + "' .")
+    if int(str(row['id'])) > 20000000 and int(str(row['id'])) < 30000000:
+        lines.append("ogham:cisp:" + str(row['id']) + " " + "rdf:type" + " oghamonto:" + "CISP" + " .")
+        lines.append("ogham:cisp:" + str(row['id']) + " " + "rdfs:label" + " 'cisp:" + str(row['id']) + "' .")
+    if int(str(row['id'])) > 30000000 and int(str(row['id'])) < 40000000:
+        lines.append("ogham:o3d:" + str(row['id']) + " " + "rdf:type" + " oghamonto:" + "OghamIn3D" + " .")
+        lines.append("ogham:o3d:" + str(row['id']) + " " + "rdfs:label" + " 'o3d:" + str(row['id']) + "' .")
+    if int(str(row['id'])) > 50000000 and int(str(row['id'])) < 60000000:
+        lines.append("ogham:squirrel:" + str(row['id']) + " " + "rdf:type" + " oghamonto:" + "SquirrelOgham" + " .")
+        lines.append("ogham:squirrel:" + str(row['id']) + " " + "rdfs:label" + " 'squirrel:" + str(row['id']) + "' .")
+    if str(row['macalister_1945']) != 'nan':
+        if str(row['macalister_1945']) != 'undefined':
+            lines.append("ogham:macalister_1945:" + str(row['macalister_1945']) + " " + "rdf:type" + " oghamonto:" + "macalister_1945" + " .")
+            lines.append("ogham:macalister_1945:" + str(row['macalister_1945']) + " " + "rdfs:label" + " 'macalister_1945:" + str(row['macalister_1945']) + "' .")
+            lines.append("ogham:ciic:" + str(row['id']) + " " + "oghamonto:equals" + " " + "ogham:macalister_1945:" + str(row['macalister_1945']) + "" + ".")
     lines.append("")
 
 files = (len(lines) / 100000) + 1
 print("triples", len(lines), "files", int(files))
+thiscount = len(lines)
+_config.count(thiscount)
 
 # write output files
 f = 0
